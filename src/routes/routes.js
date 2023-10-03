ad1aa24772e9729dbf94a6d484a2c897600caab1
Ricardo Alves <ralvespiedade@gmail.com>
2023-10-03 15:31:31 -0300

    adicionando metodo put

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ const ProductController = require('../controllers/products')
 
 router.get('/products/:id?', ProductController.get)
 router.post('/products/:id?', ProductController.post)
+router.put('/products/:id', ProductController.put)
 
 
 module.exports = router
